94a5a1bf0b00f49b6b294368df1af1f046bb01fa
Radium <47716215+farisabit@users.noreply.github.com>
2021-01-19 23:34:38 +0100

    Create debiasing_facial_detection_sys.py

---
+++ debiasing_facial_detection_sys.py
@@ -0,0 +1,21 @@
+""" # Copyright 2021 MIT 6.S191 Introduction to Deep Learning. All Rights Reserved.
+# 
+# Licensed under the MIT License. You may not use this file except in compliance
+# with the License. Use and/or modification of this code outside of 6.S191 must
+# reference:
+#
+# © MIT 6.S191: Introduction to Deep Learning
+# http://introtodeeplearning.com
+#
+"""
+import tensorflow as tf
+
+import IPython
+import functools
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+# Download and import the MIT 6.S191 package
+!pip install mitdeeplearning
+import mitdeeplearning as mdl
