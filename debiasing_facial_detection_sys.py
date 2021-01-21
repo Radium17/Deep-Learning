bc6852898945d080bbea3104eacef25efe385ee6
Radium <47716215+farisabit@users.noreply.github.com>
2021-01-22 00:15:15 +0100

    Update debiasing_facial_detection_sys.py

--- debiasing_facial_detection_sys.py
+++ debiasing_facial_detection_sys.py
@@ -133,15 +133,122 @@ plt.xticks(xx, keys)
 plt.ylim(max(0,yy.min()-yy.ptp()/2.), yy.max()+yy.ptp()/2.)
 plt.title("Standard classifier predictions")
 
-"""  In learning the latent space, we constrain the means and standard deviations to approximately follow a unit Gaussian. 
-     Recall that these are learned parameters, and therefore must factor into the loss computation, and that the decoder portion of 
-     the VAE is using these parameters to output a reconstruction that should closely match the input image, which also must factor 
-     into the loss. What this means is that we'll have two terms in our VAE loss function:
+""" 
+In learning the latent space, we constrain the means and standard deviations to approximately follow a unit Gaussian. 
+Recall that these are learned parameters, and therefore must factor into the loss computation, and that the decoder portion of 
+the VAE is using these parameters to output a reconstruction that should closely match the input image, which also must factor 
+into the loss. What this means is that we'll have two terms in our VAE loss function:
 
 1.  **Latent loss ($L_{KL}$)**: measures how closely the learned latent variables match a unit Gaussian and is defined by the Kullback-Leibler (KL) divergence.
 2.  **Reconstruction loss ($L_{x}{(x,\hat{x})}$)**: measures how accurately the reconstructed outputs match the input and is given by the $L^1$ norm of
     the input image and its reconstructed output.
 """
+
+# Defining the VAE loss function
+def vae_loss_function(x, x_recon, mu, logsigma, kl_weight=0.0005):
+  latent_loss = 0.5*tf.reduce_sum(tf.exp(logsigma)+tf.square(mu)-1-logsigma, axis=1)  #Kullback-Leibler (KL) divergence
+  reconstruction_loss = tf.reduce_sum(tf.abs(x-x_recon), axis=(1,2,3))
+  vae_loss = kl_weight*latent_loss + reconstruction_loss
+  
+  return vae_loss
+
+# VAE Reparametrization by sampling from an isotropic unit Gaussian
+def sampling(z_mean, z_logsigma):
+  """
+  z_mean, z_logsigma (tensor): mean and log of std of latent distribution (Q(z|X))
+  returns z(tensor): sampled latent vector
+  """
+  
+  batch, latent_dim = z_mean.shape
+  epsilon = tf.random.normal(shape=(batch, latent_dim))
+  z = z_mean + tf.exp(0.5*z_logsigma)*epsilon
+  
+  return z
+
+# Loss function for DB-VAE
+def debiasing_loss_function(x, x_recon, y, y_logit, mu, logsigma):
+  
+  vae_loss = vae_loss_function(x, x_recon, mu, logsigma, kl_weight=0.0005)
+  classification_loss = tf.nn.sigmoid_cross_entropy_with_logits(y, y_logit)
+  face_indicator = tf.cast(tf.equal(y,1), tf.float32)
+  total_loss = tf.reduce_mean(classification_loss + face_indicator*vae_loss)
+  
+  return total_loss, classification_loss
+
+# Decoder part of the dB-DAE
+n_filters = 12
+latent_dim = 100
+
+def make_face_decoder_network():
+  
+  Conv2DTranspose = functools.partial(tf.keras.layers.Conv2DTranspose, padding='same', activation='relu')
+  BatchNormalization = tf.keras.layers.BatchNormalization
+  Flatten = tf.keras.layers.Flatten
+  Dense = functools.partial(tf.keras.layers.Dense, activation='relu')
+  Reshape = tf.keras.layers.Reshape
+  
+  # Build the decoder network
+  decoder = tf.keras.Sequential([
+    # Transform to pre-convolutional generation
+    Dense(units=4*4*6*n_filters), # 4x4 feature matrix with 6N occurances
+    Reshape(target_shape=(4,4,6*n_filters)),
+    
+    # Upscaling convolutions (inverse to encoder)
+    Conv2DTranspose(filters=4*n_filters, kernel_size=3, strides=2),
+    Conv2DTranspose(filters=2*n_filters, kernel_size=3, strides=2),
+    Conv2DTranspose(filters=1*n_filters, kernel_size=5, strides=2),
+    Conv2DTranspose(filters=3, kernel_size=5, strides=2),
+  ])
+  
+  return decoder
+
+# Defining and creating the DB-VAE
+class DB_VAE(tf.keras.Model):
+  
+  def __init__(self, latent_dim):
+    super(DB_VAE, self).__init__()
+    self.latent_dim = latent_dim
+    
+    # `latent_dim` latent variables & a supervised output for the classification
+    num_encoder_dims = 2*self.latent_dims + 1
+    
+    self.encoder = make_standard_classifier(num_encoder_dims)
+    self.decoder = make_face_decoder_network()
+    
+  def encode(self, x):
+    encoder_output = self.encoder(x)
+    # classification prediction
+    y_logit = tf.expand_dims(encoder_output[:,0], -1)
+    # latent variable distribution parameters
+    z_mean = encoder_output[:, 1:self.latent_dim+1]
+    z_logsigma = encoder_output[:, self.latent_dim+1:]
+    
+    return y_logit, z_mean, z_logsigma
+  
+  # VAE reparametrization: given a mean and logsigma, sample latent variables
+  def reparameterize(self, z_mean, z_logsigma):
+    return sampling(z_mean, z_logsigma)
+  
+  # Decode the latent space and output reconstruction
+  def decode(self, z):
+    return self.decoder(z)
+    
+  def call(self, x):
+    y_logit, z_mean, z_logsigma = self.encode(x)
+    z = self.reparameterize(z_mean, z_logsigma)
+    recon = self.decode(z)
+    
+    return y_logit, z_mean, z_logsigma, recon
+  
+  def predict(self, x):
+    y_logit, _, _ = self.encode(x)
+    return y_logit
+  
+dbvae = DB_VAE(latent_dim)
+
+
+
+
 
 
 
