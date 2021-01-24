7b323c032b6050571de14632c6ebf3772d3f9877
Radium <47716215+farisabit@users.noreply.github.com>
2021-01-24 22:49:23 +0100

    Update reinforcement-learning.py

--- reinforcement-learning.py
+++ reinforcement-learning.py
@@ -126,30 +126,130 @@ def train_step(model, optimizer, observations, actions, discounted_rewards):
   grads = tape.gradient(loss, model.trainable_variables)
   optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Cartpole training
+learning_rate = 1e-3
+optimizer = tf.keras.optimizers.Adam(learning_rate)
+cartpole_model = create_model()
+
+# to track the progress
+smoothed_reward = mdl.util.LossHistory(smoothing_factor = 0.9)
+plotter = mdl.util.PeriodicPlotter(sec=2, xlabel='Iterations', ylabel='Rewards')
+if hasattr(tqdm, '_instances'): tqdm._instances.clear()
+for i_episode in range(500):
+  plotter.plot(smoothed_reward.get())
+  
+  observation = env.reset()
+  memory.clear()
+  
+  while True:
+    action = choose_action(cartpole_model, observation)
+    next_observation, reward, done, info = env.step(action)
+    memory.add_to_memory(observation, action, reward)
+    
+    # is the episode over? did you crash or did you so well that you're done?
+    if done:
+      total_reward = sum(memory.rewards)
+      smoothed_reward.append(total_reward)
+      
+      # initiate training - we don't know anything about how the agent
+      # is doing until it has crashed 
+      train_step(cartpole_model, optimizer,
+                observations = np.vstack(memory.observations),
+                actions = np.array(memory.actions),
+                discounted_rewards = discount_rewards(memory.rewards))
+      
+      memory.clear()
+      break
+    observation = next_observation
+    
+    # to watch how the agent did
+    saved_cartpole = mdl.lab3.save_video_of_model(cartpole_model, "CartPole-v0")
+    mdl.lab3.play_video(saved_cartpole)
 
 ### PART 2: Pong
+
+def create_pong_env():
+  return gym.make("Pong-v0", frameskip=5)
+env = create_pong_env()
+env.seed(1) 
+
+# In the case of Pong our observations are the individual video frames(i.e. images)
+# Thus, the observations are 210x160 RGB
+print("Environment has observation space =", env.observation_space)
+
+# 6 actions: no-op, move right/left, fire, fire right/left
+n_actions = env.action_space.n
+print("Number of possible actions that the agent can choose from =", n_actions)
+
+# Define the Pong agent
+Conv2D = functools.partial(tf.keras.layers.Conv2D, padding='same', activation='relu')
+Flatten = tf.keras.layers.Flatten
+Dense = tf.keras.layers.Dense
+
+def create_pong_model():
+  model = tf.keras.models.Sequential([
+    Conv2D(filters=32, kernel_size=5, stride=2),
+    Conv2D(filters=48, kernel_size=5, stride=2),
+    Conv2D(filters=64, kernel_size=3, stride=2),
+    Conv2D(filters=64, kernel_size=3, stride=2),
+    Flatten(),
+    Dense(units=128, activation='relu'),
+    Dense(units=n_actions, activation='None')
+  ])
+  return model
+
+pong_model = create_pong_model()
+
+# Pong reward function
+def discount_rewards(rewards, gamma=0.99):
+  discounted_rewards = np.zeros_like(rewards)
+  R = 0
+  for t in reversed(range(0, len(rewards))):
+    # reset the sum if the reward is not 0 (the game has ended)
+    if rewards[t] != 0:
+      R = 0
+    R = R*gamma + rewards[t]
+    discounted_rewards[t] = R
+    
+  return normalize(discounted_rewards)
+
+# To visualize a single observation before and after pre-processing
+observation = env.reset()
+for i in range(30):
+  action = np.random.choice(n_actions)
+  observation, _, _, _ = env.step(action)
+observation_pp = mdl.lab3.preprocess_pong(observation)
+
+f = plt.figure(figsize=(10,3))
+ax = f.add_subplot(121)
+ax2 = f.add_subplot(122)
+ax.imshow(observation); ax.grid(False)
+ax2.imshow(np.squeeze(observation_pp)); ax2.grid(False);
+plt.title('Preprocessed Observation')
+
+# To capture the dynamics, i.e. how the environment changes
+next_observation, _, _, _ = env.step(np.random.choice(n_actions))
+diff = mdl.lab3.pong_change(observation, next_observation)
+
+f, ax = plt.subplot(1, 3, figsize=(15,15))
+for a in ax:
+  a.grid(False)
+  a.axis("off")
+ax[0].imshow(observation); ax[0].set_title('Previous Frame');
+ax[1].imshow(next_observation); ax[1].set_title('Current Frame');
+ax[2].imshow(np.squeeze(diff)); ax[2].set_title('Difference (Model Input)');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
